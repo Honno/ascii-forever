f2195d122ffabcea5be09922283670fe966b4905
Matthew Barber <quitesimplymatt@gmail.com>
2021-03-19 13:56:54 +0000

    basic upload

--- apps/foo/urls.py
+++ apps/foo/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 
 from .views import *
 
+
 app_name = "foo"
-urlpatterns = [path("", IndexView.as_view(), name="index")]
+urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
+    path("upload/", UploadView.as_view(), name="upload"),
+]
--- apps/foo/views.py
+++ apps/foo/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, reverse
+from django.views.generic import ListView, CreateView
 from django.utils import timezone
 
 from .models import Art
 
-__all__ = ["IndexView"]
+__all__ = ["IndexView", "UploadView"]
 
 
 class IndexView(ListView):
@@ -17,3 +18,23 @@ class IndexView(ListView):
             .order_by("-timestamp")[:10]
         )
 
+
+class UploadView(CreateView):
+    model = Art
+    fields = ["text"]
+    template_name = "upload.html"
+
+    def post(self, req):
+        try:
+            text = req.POST["text"]
+        except KeyError:
+            return render(
+                req,
+                "upload.html",
+                {"err": "No text was uploaded :o"},
+            )
+
+        art = Art(text=text, timestamp=timezone.now())
+        art.save()
+
+        return HttpResponseRedirect(reverse("foo:index"))
