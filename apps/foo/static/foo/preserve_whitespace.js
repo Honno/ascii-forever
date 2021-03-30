5d9abb75c820a614b03db4d14424d5fe40962709
Matthew Barber <quitesimplymatt@gmail.com>
2021-03-30 10:24:26 +0100

    remove programmatic dot prepends on form errors

--- apps/foo/static/foo/preserve_whitespace.js
+++ apps/foo/static/foo/preserve_whitespace.js
@@ -1,6 +1,16 @@
 var form = document.querySelector("form.preserve-whitespace");
 var textarea = form.querySelector("textarea");
 
+// if a server-side error occurs, the page is POST'd to
+// browsers may carry over the dot that was prepended previously
+// so we remove any such dot
+// note that this may remove a legitimate dot ¯\_(ツ)_/¯
+if (textarea.value.length > 0) {
+    if (textarea.value[0] == ".") {
+        textarea.value = textarea.value.substring(1);
+    }
+}
+
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     textarea.value = "." + textarea.value;
